e99d19be00a8787140bac6689b48a27bd5f5029e
codewizz <c.chaiwat.ch@gmail.com>
2022-01-17 18:03:16 +0000

    update: method find by author lastname

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -11,5 +11,5 @@ public interface PostRepository extends JpaRepository<Post, Long> {
   @RestResource(rel="contains-title", path="containsTitle")
   public List<Post> findByTitleContaining (String title);
 
-  public List<Post> findByAuthorLastname (String lastname);
+  public List<Post> findByAuthor_Lastname (String lastname);
 }
