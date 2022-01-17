09ae9baf6587897efe61a3d36661d23f2d5e4669
codewizz <c.chaiwat.ch@gmail.com>
2022-01-17 18:00:49 +0000

    del: unuse decorator lab-3

--- src/main/java/com/pluralsight/blog/data/AuthorRepository.java
+++ src/main/java/com/pluralsight/blog/data/AuthorRepository.java
@@ -3,10 +3,6 @@ package com.pluralsight.blog.data;
 import com.pluralsight.blog.model.Author;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.stereotype.Component;
 
-@Component
-@RestResource
 @RepositoryRestResource(exported = false)
 public interface AuthorRepository extends JpaRepository<Author, Long> {}
